41d1768a516637bc7ac9dece094dc66e1d1a03c8
edard3v <edard3v@gmail.com>
2024-12-29 06:42:14 -0500

    mas o menos funciona

--- src/modules/products/get-products/getProductsService.ts
+++ src/modules/products/get-products/getProductsService.ts
@@ -1,23 +1,31 @@
 import { and, eq, gt, like } from "drizzle-orm";
 import { db } from "../../../db/db";
 import { GetProducts } from "./getProductsSchema";
-import { products } from "../../../db/schemas";
+import {
+  categories,
+  products,
+  productsCategories as middle,
+} from "../../../db/schemas";
 import { DtoErr } from "../../../errors/DtoErr";
 
 export const getProductsService = async (params: GetProducts = {}) => {
-  const { page = 1, limit = 20, name } = params;
+  const { page = 1, limit = 20, name, categoryId } = params;
 
   const where = [
     name ? like(products.name, `%${name}%`) : undefined,
     eq(products.isActive, true),
     gt(products.stock, 0),
+    categoryId ? eq(middle.categoryId, categoryId) : undefined,
   ].filter(Boolean);
 
   const totalRecords = (
     await db
       .select({ id: products.id })
       .from(products)
+      .leftJoin(middle, eq(middle.productId, products.id))
+      .leftJoin(categories, eq(categories.id, middle.categoryId))
       .where(and(...where))
+      .groupBy(products.id)
   ).length;
 
   const totalPages = Math.ceil(totalRecords / limit) || 1;
@@ -26,14 +34,33 @@ export const getProductsService = async (params: GetProducts = {}) => {
   const records = await db
     .select()
     .from(products)
+    .leftJoin(middle, eq(middle.productId, products.id))
+    .leftJoin(categories, eq(categories.id, middle.categoryId))
     .where(and(...where))
     .limit(limit)
     .offset((page - 1) * limit);
+
+  const recordsFormated = [];
+  for (let i = 0; i < records.length; i++) {
+    const tuple = records[i];
+    const { categories: category, products } = tuple;
+
+    const recordFound = recordsFormated.find((item) => item.id === products.id);
+
+    if (recordFound?.id) {
+      recordFound.categories.push(category);
+    }
+
+    recordsFormated.push({
+      ...products,
+      categories: [category].filter(Boolean),
+    });
+  }
 
   return {
     limit,
     page,
     totalPages,
-    records,
+    records: recordsFormated,
   };
 };
