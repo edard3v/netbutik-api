00b0628ae0d2e28b0bc17c828fb806b0447bc680
edard3v <edard3v@gmail.com>
2025-01-02 14:08:41 -0500

    agg isProduction

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,9 +5,12 @@ import { createClient } from "@libsql/client";
 import * as schemas from "./schemas";
 import * as relations from "./relations";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const CONFIG_DB = {
-  url: "file:./src/db/netbutik.db",
-  // url: process.env.TURSO_CONNECTION_URL!,
+  url: !isProduction
+    ? "file:./src/db/netbutik.db"
+    : process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 };
 
