874a844511910257d9d50ba0bbb1adf4c7fb7c2e
edard3v <edard3v@gmail.com>
2025-01-02 06:02:44 -0500

    refactor

--- src/db/drafts/categories.draft.ts
+++ src/db/drafts/categories.draft.ts
@@ -1,47 +1,47 @@
 export const CATEGORIES = [
   {
     id: "c1000000-0000-0000-0000-000000000000",
-    name: "sueteres",
+    name: "Sueter",
     img: "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c2000000-0000-0000-0000-000000000000",
-    name: "pantalones",
+    name: "Pantalón",
     img: "https://images.pexels.com/photos/1082528/pexels-photo-1082528.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c3000000-0000-0000-0000-000000000000",
-    name: "medias",
+    name: "Media",
     img: "https://images.pexels.com/photos/1057005/pexels-photo-1057005.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c4000000-0000-0000-0000-000000000000",
-    name: "camisas",
+    name: "Camisa",
     img: "https://images.pexels.com/photos/264726/pexels-photo-264726.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c5000000-0000-0000-0000-000000000000",
-    name: "zapatos",
+    name: "Zapatos",
     img: "https://images.pexels.com/photos/18799795/pexels-photo-18799795/free-photo-of-flores-jardin-zapatos-caracol.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c6000000-0000-0000-0000-000000000000",
-    name: "bolsos",
+    name: "Bolso",
     img: "https://images.pexels.com/photos/3908800/pexels-photo-3908800.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c7000000-0000-0000-0000-000000000000",
-    name: "reloj",
+    name: "Reloj",
     img: "https://images.pexels.com/photos/14675876/pexels-photo-14675876.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c8000000-0000-0000-0000-000000000000",
-    name: "gorra",
+    name: "Gorra",
     img: "https://images.pexels.com/photos/6342793/pexels-photo-6342793.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
   {
     id: "c9000000-0000-0000-0000-000000000000",
-    name: "chaqueta",
+    name: "Chaqueta",
     img: "https://images.pexels.com/photos/2765557/pexels-photo-2765557.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1",
   },
 ];
